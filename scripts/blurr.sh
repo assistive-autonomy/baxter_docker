bdb8c3a4403695cb60621a524b5ac33be98cd527
Emanuele <emanuele.depellegrin@protonmail.com>
2026-03-13 17:19:17 +0000

    env changes and init script

--- scripts/blurr.sh
+++ scripts/blurr.sh
@@ -19,11 +19,11 @@ unset your_hostname
 # will be overwritten by any variables set here.
 
 # Specify Baxter's hostname
-baxter_hostname="011308p0016.local"
+baxter_hostname="$ROBOT_HOSTNAME"
 
 # Set *Either* your computers ip address or hostname. Please note if using
 # your_hostname that this must be resolvable to Baxter.
-your_ip="192.168.106.99"
+your_ip="$YOUR_IP"
 
 # Specify ROS distribution (e.g. indigo, hydro, etc.)
 ros_version="indigo"
